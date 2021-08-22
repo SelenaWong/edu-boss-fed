ab72f398c67f88647da15ff7ba82d89d502d3293
wangxy <wangxy@amegroups.com>
2021-08-22 22:22:16 +0800

    ...

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
 <template>
   <div id="app">
     <h1>title</h1>
-<!--  根路由出口-->
+    <p class="text">test</p>
+    <!--  根路由出口-->
     <router-view/>
   </div>
 </template>
+<script lang="ts">
+import Vue from 'vue'
+import request from '@/utils/request'
 
+request({
+  method: 'GET',
+  url: '/boss/v2/api-docs?group=edu-boss-boot'
+}).then(res => {
+  console.log(res)
+})
+
+export default Vue.extend({
+  name: 'App'
+})
+</script>
 <style lang="scss" scoped>
-
+  .text {
+    color: $success-color;
+  }
 </style>
