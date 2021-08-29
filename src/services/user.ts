d7acd2de9fb740abb355c2b78edfb062b156388e
wangxy <wangxy@amegroups.com>
2021-08-29 20:40:49 +0800

    5. 用户模块；

--- src/services/user.ts
+++ src/services/user.ts
@@ -29,3 +29,21 @@ export const getUserInfo = ():Promise<any> => {
     url: '/front/user/getInfo'
   })
 }
+
+export const getUserPages = (data: any) => {
+  return request({
+    method: 'POST',
+    url: '/boss/user/getUserPages',
+    data
+  })
+}
+
+export const forbidUser = (userId: string | number) => {
+  return request({
+    method: 'POST',
+    url: '/boss/user/forbidUser',
+    params: {
+      userId
+    }
+  })
+}
